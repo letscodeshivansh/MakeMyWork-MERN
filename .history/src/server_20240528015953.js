5a4f11fc32d6bec7b66ca63eabf82824e3e6a86c
shivansh <shivanshsrivastava0999@gmail.com>
2024-05-28 12:08:44 +0530

    by karan
    karan navbar

---
+++ .history/src/server_20240528015953.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+
+const port = 300;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
